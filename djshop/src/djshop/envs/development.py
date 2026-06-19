e1951ad894b1a3e55150120296ae05d68637a117
m.garmabi <m.garmabi@khodro45.com>
2026-06-19 13:32:53 +0330

    Remove unnecessary README file to streamline project structure.

---
+++ djshop/src/djshop/envs/development.py
@@ -0,0 +1,20 @@
+import os
+
+from .common import *
+
+INSTALLED_APPS = [
+  "daphne",
+  "drf_spectacular",
+] + INSTALLED_APPS
+
+
+DATABASES = {
+  "default": {
+    "ENGINE": "django.db.backends.postgresql",
+    "NAME": os.environ.get("POSTGRES_DB", "djshop"),
+    "USER": os.environ.get("POSTGRES_USER", "djshop"),
+    "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "123@456"),
+    "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
+    "PORT": os.environ.get("POSTGRES_PORT", "5432"),
+  }
+}
